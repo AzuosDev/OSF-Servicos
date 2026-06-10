14ee2a5fb526826051b7d3e77b9448f1b15e916c
Felipe Souza <235802449+felipedes687-dotcom@users.noreply.github.com>
2026-06-10 19:08:58 -0300

    feat:  add speedinsights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import "./styles.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -25,6 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <App />
+          <SpeedInsights/>
         </BrowserRouter>
       </QueryClientProvider>
     </ThemeProvider>
