2e3e55644ff46ec203aaf4728d9d7cc172cd6a2a
Felipe Souza <235802449+felipedes687-dotcom@users.noreply.github.com>
2026-06-10 19:34:41 -0300

    fix: build speedinsight

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ import { RegisterPage } from "./pages/RegisterPage";
 import { ResetPasswordPage } from "./pages/ResetPasswordPage";
 import { TransactionsPage } from "./pages/TransactionsPage";
 import { VerifyEmailPage } from "./pages/VerifyEmailPage";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function PrivateRoute() {
   const location = useLocation();
@@ -79,9 +79,8 @@ export default function App() {
             </Route>
 
             <Route path="*" element={<Navigate to="/" replace />} />
-
-            <SpeedInsights/>
           </Routes>
+          <SpeedInsights />
         </AppBoot>
       </ToastProvider>
     </ThemeProvider>
