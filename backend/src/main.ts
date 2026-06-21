95d17bec4066623183a487a7a8666e2a991bbf5a
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2026-06-21 12:46:35 -0300

    feat: update api name main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -52,7 +52,7 @@ async function bootstrap() {
   if (process.env.NODE_ENV !== 'production') {
     const swaggerConfig = new DocumentBuilder()
       .setTitle('ContaCerta API')
-      .setDescription('ContaCerta backend API')
+      .setDescription('MeuGasto backend API')
       .setVersion('1.0')
       .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'Authorization')
       .build();
